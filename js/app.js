41f7a0e35b403708065f4b9f39281936e31d83fa
Evgun1 <eugenegavrick@gmail.com>
2023-02-02 18:02:33 +0200

    update

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@
  * 2) підписаьтися у нього на Event click
  * 3) під час цього івенту переключати стан панелі(прихована відображена). Перехід має бути анімованим
  */
+const btnBack = document.querySelector('.header__button-back')
+let item = document.querySelector(".sidenav")
+
+btnBack.addEventListener('click', function () {
+    item.classList.toggle("sidenav__action");
+})
+
+btnBack.addEventListener('click', function () {
+    let btnBack = document.querySelector('.arrow-back')
+    btnBack.classList.toggle("arrow-forth");
+})
